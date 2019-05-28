ec30bdeef92a547a7609bb73882fc55e9981bcac
Antonio J. Franceschi <francofle@hotmail.com>
2019-05-28 10:20:17 -0400

    Completed lecture 229-Building Our Components

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return(
+        <nav className='flex justify-end'>
+            <p className='pa3 f3 link pointer underline dark-gray dim'>Sign Out</p>
+        </nav>
+    )
+};
+
+export default Navigation;
